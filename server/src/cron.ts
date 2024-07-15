bac16f354f68525bd1ab6b9246b75d811ac28336
Sachin Kumar <sachin89359@gmail.com>
2024-07-16 01:02:37 +0530

    Add files via upload

---
+++ server/src/cron.ts
@@ -0,0 +1,28 @@
+import { CronJob } from "cron";
+import https from "https";
+
+const backendUrl: string =
+  "https://real-time-collaborative-coding-platform-1.onrender.com";
+
+const job: CronJob = new CronJob("*/14 * * * * *", function () {
+  // This function will be executed every 14 seconds.
+  console.log("Restarting server");
+
+  // Perform an HTTPS GET request to hit any backend API.
+  https
+    .get(backendUrl, (res) => {
+      if (res.statusCode === 200) {
+        console.log("Server restarted");
+      } else {
+        console.error(
+          `Failed to restart server with status code: ${res.statusCode}`
+        );
+      }
+    })
+    .on("error", (err: Error) => {
+      console.error("Error during Restart:", err.message);
+    });
+});
+
+// Export the cron job.
+export { job };
